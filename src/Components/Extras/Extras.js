7c71f39e6ff3d418597b915acada2f3bfe7700e9
Rohan Kadam <rohan.kadam@mindbowser.com>
2023-02-14 12:54:05 +0530

    Improve extras image slider and added image hover effect

--- src/Components/Extras/Extras.js
+++ src/Components/Extras/Extras.js
@@ -1,7 +1,8 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 import { register } from "swiper/element/bundle";
+import ArrowCircleRightOutlinedIcon from "@mui/icons-material/ArrowCircleRightOutlined";
 
 // Import Swiper styles
 import "swiper/css";
@@ -9,17 +10,20 @@ import "swiper/css/pagination";
 
 // import required modules
 import { Autoplay, Pagination, Navigation } from "swiper";
+import { EXTRAS_IMAGE_SLIDER } from "../../constant";
 
 const Extras = () => {
   register();
   return (
-    <Box mt={5}>
-      {" "}
+    <Box mt={7} mb={5}>
+      <Typography variant="h4" mb={2} sx={{ color: "#913175" }}>
+        Extras
+      </Typography>
       <Swiper
         slidesPerView={1}
         spaceBetween={10}
         autoplay={{
-          delay: 1500,
+          delay: 2000,
           disableOnInteraction: false,
         }}
         pagination={{
@@ -28,69 +32,35 @@ const Extras = () => {
         breakpoints={{
           640: {
             slidesPerView: 2,
-            spaceBetween: 20,
+            spaceBetween: 10,
           },
           768: {
             slidesPerView: 4,
-            spaceBetween: 40,
+            spaceBetween: 5,
           },
           1024: {
-            slidesPerView: 5,
-            spaceBetween: 50,
+            slidesPerView: 4,
+            spaceBetween: 10,
           },
         }}
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper"
       >
-        <SwiperSlide>
-          <img
-            src={
-              "https://www.airvistara.com/content/dam/airvistara/global/english/home-page/extras/vistara-meet-and-assist.png"
-            }
-          ></img>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src={
-              "https://www.airvistara.com/content/dam/airvistara/global/english/home-page/extras/vistara-meet-and-assist.png"
-            }
-          ></img>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src={
-              "https://www.airvistara.com/content/dam/airvistara/global/english/home-page/extras/vistara-meet-and-assist.png"
-            }
-          ></img>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src={
-              "https://www.airvistara.com/content/dam/airvistara/global/english/home-page/extras/vistara-meet-and-assist.png"
-            }
-          ></img>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src={
-              "https://www.airvistara.com/content/dam/airvistara/global/english/home-page/extras/vistara-meet-and-assist.png"
-            }
-          ></img>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src={
-              "https://www.airvistara.com/content/dam/airvistara/global/english/home-page/extras/vistara-meet-and-assist.png"
-            }
-          ></img>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src={
-              "https://www.airvistara.com/content/dam/airvistara/global/english/home-page/extras/vistara-meet-and-assist.png"
-            }
-          ></img>
-        </SwiperSlide>
+        {EXTRAS_IMAGE_SLIDER.map((image, id) => {
+          return (
+            <SwiperSlide key={id}>
+              <Box sx={{ position: "relative" }}>
+                <img className="extra_image" src={image} alt="extras"></img>
+                <div className="extras_image_hover">
+                  <Typography sx={{ color: "white", fontWeight: "700" }}>
+                    Vistara Select
+                  </Typography>
+                  <ArrowCircleRightOutlinedIcon sx={{ color: "white" }} />
+                </div>
+              </Box>
+            </SwiperSlide>
+          );
+        })}
       </Swiper>
     </Box>
   );
--- src/constant/index.js
+++ src/constant/index.js
@@ -4,7 +4,16 @@ export const APP_LOGO =
 export const HOME_IMAGE_SLIDER = [
   "https://www.airvistara.com/content/dam/airvistara/in/english/2023/jan-/revise_GOX_HP_banner.png",
   "https://www.airvistara.com/content/dam/airvistara/in/english/2023/jan-/cv/Rewarding-Takeoffs-Offer-1354x450-HP-Banner.png",
-  "https://www.airvistara.com/content/dam/airvistara/saudi-arabia/dammam/IND-facing-HP.png",
+  "https://www.airvistara.com/content/dam/airvistara/in/cv/MicrosoftTeams-image-13feb.png",
   "https://www.airvistara.com/content/dam/airvistara/saudi-arabia/dammam/IND-facing-HP.png",
   "https://www.airvistara.com/content/dam/airvistara/in/english/2023/jan-/cv/Rewarding-Takeoffs-Offer-1354x450-HP-Banner.png",
+];
+
+export const EXTRAS_IMAGE_SLIDER = [
+  "https://www.airvistara.com/content/dam/airvistara/global/english/home-page/extras/hotels.jpg",
+  "https://www.airvistara.com/content/dam/airvistara/global/english/home-page/extras/vistara-select.jpg",
+  "https://www.airvistara.com/content/dam/airvistara/global/english/home-page/extras/vistara-meet-and-assist.png",
+  "https://www.airvistara.com/content/dam/airvistara/global/english/home-page/extras/vistara-excess-baggage.jpg",
+  "https://www.airvistara.com/content/dam/airvistara/global/english/home-page/extras/lounge-access.jpg",
+  "https://www.airvistara.com/content/dam/airvistara/global/english/home-page/extras/vistara-upgrade.jpg",
 ];
